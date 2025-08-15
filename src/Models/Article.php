e55b790602921b7b64dd2a6ff440d0fbb3e82fc5
00MostafaMohsen00 <geye096@gmail.com>
2025-08-15 23:36:48 +0300

    edits

--- src/Models/Article.php
+++ src/Models/Article.php
@@ -2,6 +2,7 @@
 
 namespace WireContent\Models;
 
+use Awcodes\Curator\Models\Media;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use WireComments\Traits\Commentable;
@@ -15,10 +16,16 @@ class Article extends Model
         'slug',
         'content',
         'media_id',
+        'user_id',
     ];
 
     public function User(): BelongsTo
     {
         return $this->belongsTo(config('auth.providers.users.model'));
+    }
+
+    public function image(): BelongsTo
+    {
+        return $this->belongsTo(Media::class);
     }
 }
