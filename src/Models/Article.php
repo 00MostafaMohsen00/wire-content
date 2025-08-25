7d7b99c4262632db7a74f6ae9b8317cb236e6cc7
00MostafaMohsen00 <geye096@gmail.com>
2025-08-25 15:59:41 +0300

    WireContent_25_08_2025

--- src/Models/Article.php
+++ src/Models/Article.php
@@ -2,8 +2,11 @@
 
 namespace WireContent\Models;
 
+use App\Models\Category;
+use Awcodes\Curator\Models\Media;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use WireComments\Traits\Commentable;
 
 class Article extends Model
@@ -14,10 +17,21 @@ class Article extends Model
         'slug',
         'content',
         'media_id',
+        'user_id',
     ];
 
     public function User(): BelongsTo
     {
         return $this->belongsTo(config('auth.providers.users.model'));
+    }
+
+    public function image(): BelongsTo
+    {
+        return $this->belongsTo(Media::class, 'media_id');
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'article_category');
     }
 }
