fe3a13b2e966c1facf3341582f50885e2cf5bb89
00MostafaMohsen00 <geye096@gmail.com>
2025-08-28 15:43:38 +0300

    WireContent_28_08_2025

--- src/Models/Article.php
+++ src/Models/Article.php
@@ -9,11 +9,14 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use RalphJSmit\Laravel\SEO\Support\HasSEO;
+use RalphJSmit\Laravel\SEO\Support\SEOData;
 use WireComments\Traits\Commentable;
+use Illuminate\Support\Str;
 
 class Article extends Model
 {
-    use Commentable, SoftDeletes;
+    use Commentable, SoftDeletes, HasSEO;
 
     protected $fillable = [
         'title',
@@ -39,5 +42,17 @@ class Article extends Model
     public function categories(): BelongsToMany
     {
         return $this->belongsToMany(Category::class, 'article_category');
+    }
+
+    public function getDynamicSEOData(): SEOData
+    {
+        $this->loadMissing('image');
+        $pathToFeaturedImageRelativeToPublicPath = (string)$this->image?->url;
+
+        return new SEOData(
+            title: $this->title,
+            description: tiptap_converter()->asText(Str::limit($this->content, 160)),
+            image: $pathToFeaturedImageRelativeToPublicPath ?? 'media/placeholder.png',
+        );
     }
 }
