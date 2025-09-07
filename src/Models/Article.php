33c9da5279534c439a1d1005353af8eaaca20cde
00MostafaMohsen00 <geye096@gmail.com>
2025-09-07 16:17:30 +0300

    WireContent_07_09_2025

--- src/Models/Article.php
+++ src/Models/Article.php
@@ -5,6 +5,7 @@ namespace WireContent\Models;
 use App\ArticleStatus;
 use App\Models\Category;
 use Awcodes\Curator\Models\Media;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -29,6 +30,11 @@ class Article extends Model
     protected $casts = [
         'status' => ArticleStatus::class,
     ];
+
+    public function scopeIsPublished(Builder $query): Builder
+    {
+        return $query->where('status', ArticleStatus::PUBLISHED);
+    }
 
     public function User(): BelongsTo
     {
